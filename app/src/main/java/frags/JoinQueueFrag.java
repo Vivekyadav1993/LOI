ec93d4b52eca5bfbd66363b3aa733f0d29c3d9a8
Vivekyadav1993 <vivekyadav.aza@gmail.com>
2018-03-19 15:45:32 +0530

    update

--- app/src/main/java/frags/JoinQueueFrag.java
+++ app/src/main/java/frags/JoinQueueFrag.java
@@ -2,11 +2,19 @@ package frags;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.wefika.calendar.CollapseCalendarView;
+import com.wefika.calendar.manager.CalendarManager;
+
+import org.joda.time.LocalDate;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +31,26 @@ public class JoinQueueFrag extends HelperFrags {
     @BindView(R.id.frag_join_queue_button)
     public Button mJoinQueueButton;
 
+    @BindView(R.id.frag_toolbar_title_tv)
+    public TextView mHeaderTitle;
+
+    @BindView(R.id.frag_join_queue_calendar_view)
+    public CollapseCalendarView mCalendarView;
+
+    @BindView(R.id.frag_join_queue_waiting_rl)
+    public RelativeLayout mWaitingRl;
+
+    @BindView(R.id.frag_join_queue_no_waiting_rl)
+    public RelativeLayout mNoWaitingRl;
+
+    @BindView(R.id.frag_join_queue_waiting_time_tv)
+    public TextView mJoinWaitingTime;
+
+    @BindView(R.id.frag_join_queue_pre_book_amount_tv)
+    public TextView mPreBookAmount;
+
+    @BindView(R.id.frag_join_queue_float_btn)
+    public FloatingActionButton mFloatBtn;
     public JoinQueueFrag() {
         // Required empty public constructor
     }
@@ -39,10 +67,13 @@ public class JoinQueueFrag extends HelperFrags {
 
     private void initilizerView() {
 
+      /*  CalendarManager manager = new CalendarManager(LocalDate.now(),
+                CalendarManager.State.MONTH, LocalDate.now(), LocalDate.now().plusYears(1));
+        mCalendarView.init(manager);*/
     }
 
-
-    @OnClick({R.id.frag_join_queue_button, R.id.frag_join_queue_no_waiting_button})
+    @OnClick({R.id.frag_join_queue_button, R.id.frag_join_queue_no_waiting_button ,R.id.join_queue_back_btn ,
+            R.id.frag_join_queue_float_btn})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.frag_join_queue_button:
@@ -54,7 +85,18 @@ public class JoinQueueFrag extends HelperFrags {
                 Bundle bundle1 = new Bundle();
                 replaceFrag(new JoinQueueFrag(), bundle1, JoinedQueueFrag.class.getName());
                 break;
+
+            case R.id.join_queue_back_btn:
+                getActivity().onBackPressed();
+                break;
+            case R.id.frag_join_queue_float_btn:
+                showDetailsFloatFun();
+                break;
         }
+    }
+
+    private void showDetailsFloatFun() {
+
     }
 
 
--- app/src/main/java/frags/JoinedQueueFrag.java
+++ app/src/main/java/frags/JoinedQueueFrag.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import r2stech.lifeoninternet.R;
 
 /**
@@ -16,14 +19,23 @@ import r2stech.lifeoninternet.R;
 public class JoinedQueueFrag extends Fragment {
 
 
+    @BindView(R.id.frag_joined_queue_position_in_queue_tv)
+    public TextView mPositionInQueue;
+
+    @BindView(R.id.frag_joined_queue_waiting_time_tv)
+    public TextView mWaitingTime;
+
+    @BindView(R.id.frag_toolbar_title_tv)
+    public TextView mHeaderTitle;
+
     public JoinedQueueFrag() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_joined_queue, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_joined_queue, container, false);
 
         ButterKnife.bind(this, view);
         initilizerView();
@@ -33,6 +45,39 @@ public class JoinedQueueFrag extends Fragment {
     private void initilizerView() {
 
 
+    }
+
+    @OnClick({R.id.frag_joined_queue_back_btn,R.id.frag_joined_queue_cancel_btn ,R.id.frag_joined_queue_push_back_btn,R.id.frag_joined_queue_show_details_btn})
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.frag_joined_queue_back_btn:
+                getActivity().onBackPressed();
+                break;
+
+            case R.id.frag_joined_queue_cancel_btn:
+                appointmentCancleFun();
+                break;
+            case R.id.frag_joined_queue_push_back_btn:
+                pushMySelfBackFunction();
+                break;
+
+            case R.id.frag_joined_queue_show_details_btn:
+                showDetailsFun();
+                break;
+        }
+
+    }
+
+    private void showDetailsFun() {
+
+    }
+
+    private void pushMySelfBackFunction() {
+
+    }
+
+    private void appointmentCancleFun() {
     }
 
 }
